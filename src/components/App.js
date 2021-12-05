dbd778e2282d2a7c6536729e67d6453450a70f7b
RamejaAyush <Ayushrwork@gmail.com>
2021-12-05 17:30:24 +0530

    added content

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import "../styles/app.scss";
+
+function App() {
+ return (
+  <nav>
+   <div className="logo">
+    <h1>Ayush Rameja</h1>
+   </div>
+   <div className="nav-links">
+    <h1>Smoth Scroll</h1>
+   </div>
+  </nav>
+ );
+}
+
+export default App;
