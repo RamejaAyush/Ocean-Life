ec0a9169b7e1398ab0212c62935762b0c30c4efd
RamejaAyush <Ayushrwork@gmail.com>
2021-12-09 16:46:53 +0530

    added custom hooks and images

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,28 @@
 import "../styles/app.scss";
+import images from "../images/images";
 
 function App() {
  return (
-  <nav>
-   <div className="logo">
-    <h1>Ayush Rameja</h1>
+  <div className="app">
+   <div className="main-app">
+    <nav>
+     <div className="logo">
+      <h1>Ayush Rameja</h1>
+     </div>
+     <div className="nav-links">
+      <h1>Smoth Scroll</h1>
+     </div>
+    </nav>
+    <div className="all-images">
+     <h1>Ocean Images</h1>
+     <div className="image">
+      {images.map((img) => (
+       <img className="ocean-img" src={img} alt="" />
+      ))}
+     </div>
+    </div>
    </div>
-   <div className="nav-links">
-    <h1>Smoth Scroll</h1>
-   </div>
-  </nav>
+  </div>
  );
 }
 
---
+++ src/images/images.js
@@ -0,0 +1,10 @@
+const images = [
+ "https://images.pexels.com/photos/7866211/pexels-photo-7866211.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+ "https://images.pexels.com/photos/7826551/pexels-photo-7826551.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+ "https://images.pexels.com/photos/7868374/pexels-photo-7868374.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+ "https://images.pexels.com/photos/7825843/pexels-photo-7825843.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+ "https://images.pexels.com/photos/847393/pexels-photo-847393.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+ "https://images.pexels.com/photos/7001709/pexels-photo-7001709.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+];
+
+export default images;
